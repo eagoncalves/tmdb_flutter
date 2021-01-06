66f79313d5de2f5b67646ba02336f8c3ea2c79ca
Eloi André Gonçalves <eloi.andre@hotmail.com>
2021-01-06 20:55:27 -0300

    Added the file of tests

---
+++ test/widget_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_tmdb/main.dart';
+
+void main() {
+  testWidgets('Find a text on main screen', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Text('Home'),
+      ),
+    ));
+
+    expect(find.text('Home'), findsOneWidget);
+    // expect(find.text('M'), findsNothing);
+  });
+}
